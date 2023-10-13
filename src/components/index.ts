f8c05fbbbb80c95d5dbc72f33d039f4c04722925
rapon1kt <shinnatensei@gmail.com>
2023-10-12 23:21:20 -0300

    feat: export functions directly of components folder

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./navbar/navbar";
+export { default as Hero } from "./hero/hero";
+export { default as Features } from "./features/features";
+export { default as PatchNotes } from "./patch-notes/patch-notes";
+export { default as Download } from "./download/download";
+export { default as Footer } from "./footer/footer";
