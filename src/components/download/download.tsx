370056a904d6cd46b0369b6520655a4a3ac3110a
rapon1kt <shinnatensei@gmail.com>
2023-10-12 23:18:06 -0300

    feat: create download component

---
+++ src/components/download/download.tsx
@@ -0,0 +1,45 @@
+import { East, FileDownload } from "@mui/icons-material";
+import { Container, Typography, Button, Stack } from "@mui/joy";
+
+function Download() {
+	return (
+		<Container maxWidth="lg" sx={{ textAlign: "center", my: 16 }}>
+			<Typography level="h1">
+				Start using <Typography sx={{ color: "#710000" }}>Medinik</Typography>{" "}
+				Software now
+			</Typography>
+			<Typography level="body-lg" sx={{ mb: 4 }}>
+				Ooh! We forgot to mention, it's completely free :)
+			</Typography>
+			<Stack
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "center",
+					gap: 4,
+				}}
+			>
+				<Button
+					variant="soft"
+					color="neutral"
+					size="lg"
+					sx={{ background: "#710000", width: "15rem" }}
+					endDecorator={<East />}
+				>
+					Register now
+				</Button>
+				<Button
+					variant="outlined"
+					color="neutral"
+					size="lg"
+					sx={{ borderColor: "#710000", width: "15rem" }}
+					endDecorator={<FileDownload />}
+				>
+					Download
+				</Button>
+			</Stack>
+		</Container>
+	);
+}
+
+export default Download;
