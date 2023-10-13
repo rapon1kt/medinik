e644bb2b88417a578dce443f474177cc445345fa
rapon1kt <shinnatensei@gmail.com>
2023-10-12 23:17:00 -0300

    feat: create hero component

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,50 @@
+import { Button, Container, Stack, Typography, Box } from "@mui/joy";
+
+function Hero() {
+	return (
+		<Container
+			maxWidth="lg"
+			sx={{
+				display: "flex",
+				flexDirection: "row",
+				alignItems: "center",
+				height: "90vh",
+			}}
+		>
+			<Box sx={{ width: "60%" }}>
+				<Typography level="h2">Easily and fast</Typography>
+				<Typography level="h1" gutterBottom>
+					Technology software
+				</Typography>
+				<Typography level="body-lg" sx={{ width: "60%", mb: 3 }}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+					scelerisque accumsan fermentum. Proin at risus pharetra, bibendum nunc
+					eget, pellentesque dui. Nunc porta nulla at elementum placerat.
+					Suspendisse gravida, justo in facilisis rutrum, magna odio
+					sollicitudin mauris, non maximus ex mi ac risus.
+				</Typography>
+				<Stack sx={{ display: "flex", flexDirection: "row", gap: 3, mb: 2 }}>
+					<Button
+						size="lg"
+						sx={{ background: "linear-gradient(45deg, #cc0000, #710000)" }}
+					>
+						Get Started
+					</Button>
+					<Button size="lg" color="neutral" variant="outlined">
+						Download
+					</Button>
+				</Stack>
+				<Typography level="body-sm">
+					Supported on Windows, Linux, and macOS
+				</Typography>
+			</Box>
+			<img
+				src="software-example.png"
+				alt="software-example"
+				style={{ width: "40%" }}
+			/>
+		</Container>
+	);
+}
+
+export default Hero;
