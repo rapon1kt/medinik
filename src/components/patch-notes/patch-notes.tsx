58f5f363ca0277ae83072dfa21ee86279c73f669
rapon1kt <shinnatensei@gmail.com>
2024-03-27 23:17:50 -0300

    feat: add responsive code for patch-notes component

--- src/components/patch-notes/patch-notes.tsx
+++ src/components/patch-notes/patch-notes.tsx
@@ -1,18 +1,28 @@
 import { Container, Chip, Typography, Box } from "@mui/joy";
 import { Whatshot, East } from "@mui/icons-material";
+import { useMediaQuery } from "@mui/material";
 
 function PatchNotes() {
+	const media = useMediaQuery("(max-width: 976px)");
+
 	return (
 		<Container
 			maxWidth="lg"
 			sx={{
 				display: "flex",
 				flexDirection: "row",
-				justifyContent: "space-between",
+				justifyContent: !media ? "space-between" : "center",
+				textAlign: !media ? "" : "center",
 				my: 5,
 			}}
 		>
-			<Box>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+				}}
+			>
 				<Chip
 					size="lg"
 					sx={{ mb: 2 }}
@@ -20,13 +30,20 @@ function PatchNotes() {
 				>
 					Patch Notes v1.9.2
 				</Chip>
-				<Typography level="h1" sx={{ width: "30rem", mb: 4 }}>
+				<Typography level={"h1"} sx={{ mb: 4 }}>
 					The <Typography sx={{ color: "#710000" }}>latest version </Typography>
 					of software is always waiting for you
 				</Typography>
+				{media && (
+					<img
+						src="patch-notes.png"
+						alt="patch-notes"
+						style={{ width: 350, height: 320 }}
+					/>
+				)}
 				<Typography
-					level="body-lg"
-					sx={{ width: "36rem", mb: 3, fontSize: 20 }}
+					level={!media ? "body-lg" : "body-md"}
+					sx={{ width: "100%", mb: 3 }}
 				>
 					Proin at risus pharetra, bibendum nunc eget, pellentesque dui. Nunc
 					porta nulla at elementum placerat. Suspendisse gravida, justo in
@@ -48,12 +65,13 @@ function PatchNotes() {
 					Read the latest version of change log
 				</Typography>
 			</Box>
-
-			<img
-				src="patch-notes.png"
-				alt="patch-notes"
-				style={{ width: 400, height: 360 }}
-			/>
+			{!media && (
+				<img
+					src="patch-notes.png"
+					alt="patch-notes"
+					style={{ width: 400, height: 360 }}
+				/>
+			)}
 		</Container>
 	);
 }
