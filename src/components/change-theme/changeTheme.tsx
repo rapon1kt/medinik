0d0292770e85726e16d909167b8489f2cf78a21a
rapon1kt <shinnatensei@gmail.com>
2024-05-08 22:16:27 -0300

    refactor: envolve changeTheme component with cssVarsProvider

--- src/components/change-theme/changeTheme.tsx
+++ src/components/change-theme/changeTheme.tsx
@@ -1,12 +1,17 @@
+"use client";
 import { DarkModeRounded, LightModeRounded } from "@mui/icons-material";
-import { IconButton, IconButtonProps, useColorScheme } from "@mui/joy";
+import {
+	CssVarsProvider,
+	IconButton,
+	IconButtonProps,
+	useColorScheme,
+} from "@mui/joy";
 import React from "react";
 
-export default function ChangeTheme(props: IconButtonProps) {
+function ToggleTheme(props: IconButtonProps) {
 	const { onClick, ...other } = props;
 	const { mode, setMode } = useColorScheme();
 	const [mounted, setMounted] = React.useState(false);
-
 	React.useEffect(() => setMounted(true), []);
 
 	return (
@@ -23,5 +28,13 @@ export default function ChangeTheme(props: IconButtonProps) {
 		>
 			{mode === "light" ? <DarkModeRounded /> : <LightModeRounded />}
 		</IconButton>
+	);
+}
+
+export default function ChangeTheme() {
+	return (
+		<CssVarsProvider>
+			<ToggleTheme />
+		</CssVarsProvider>
 	);
 }
