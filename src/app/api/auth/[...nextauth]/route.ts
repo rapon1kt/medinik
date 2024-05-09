b9da5e7dbb9ca9ee2d2639e02540553433930d6f
rapon1kt <shinnatensei@gmail.com>
2024-05-09 01:02:29 -0300

    fix: route bug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import { authOptions } from "../../config/authOptions";
 
-export const nextAuthOptions: NextAuthOptions = {
-	providers: [
-		Credentials({
-			name: "Credentials",
-			credentials: {
-				email: { label: "Email", type: "text" },
-				password: { label: "Password", type: "password" },
-			},
-			async authorize(credentials, req) {
-				const response = await fetch("http://localhost:8884/auth/login", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						email: credentials?.email,
-						password: credentials?.password,
-					}),
-				});
-
-				const user = await response.json();
-
-				if (user && response.ok) {
-					return user;
-				} else {
-					return;
-				}
-			},
-		}),
-		GithubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID!,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-		}),
-	],
-	pages: { signIn: "/login" },
-	callbacks: {
-		async jwt({ token, user }) {
-			user && (token.user = user);
-			return token;
-		},
-		async session({ session, token }) {
-			session = token.user as any;
-			return session;
-		},
-	},
-};
-
-const handler = NextAuth(nextAuthOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ src/app/api/config/authOptions.ts
@@ -0,0 +1,50 @@
+import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+	providers: [
+		Credentials({
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				const response = await fetch("http://localhost:8884/auth/login", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						email: credentials?.email,
+						password: credentials?.password,
+					}),
+				});
+
+				const user = await response.json();
+
+				if (user && response.ok) {
+					return user;
+				} else {
+					return;
+				}
+			},
+		}),
+		GithubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID!,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+		}),
+	],
+	pages: { signIn: "/login" },
+	callbacks: {
+		async jwt({ token, user }) {
+			user && (token.user = user);
+			return token;
+		},
+		async session({ session, token }) {
+			session = token.user as any;
+			return session;
+		},
+	},
+};
