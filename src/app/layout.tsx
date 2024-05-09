807d8859b2570efc62b7e53c9ea00de9535e4174
rapon1kt <shinnatensei@gmail.com>
2024-05-08 22:17:27 -0300

    feat: envolve app with themeProvider provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ThemeProvider } from "@/provider";
 
 const poppins = Poppins({
 	subsets: ["latin"],
@@ -22,7 +23,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="pt-br">
-			<body className={poppins.className}>{children}</body>
+			<ThemeProvider>
+				<body className={poppins.className}>{children}</body>
+			</ThemeProvider>
 		</html>
 	);
 }
